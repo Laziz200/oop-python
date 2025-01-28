4d5d4a53aaf3504344408e7239ced8246e900137
Laziz200 <lazizshakarov243@gmail.com>
2025-01-28 14:52:02 +0500

    .

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ class CardType:
 
 
 class Card:
-    def __init__(self, name: str, number: int, cvv: int, exapry_date: datetime, money: float, type: CardType):
+    def __init__(self, name: str, number: int, cvv: int, expary_date: datetime, money: float, type: CardType):
         self.name = name
         self.number = number
         self.cvv = cvv
-        self.expary_date = exapry_date
+        self.expary_date = expary_date
         self.money = money
         self.type = type
 
@@ -43,46 +43,54 @@ class Account:
         s = 0
         for card in self.__cards:
             s += card.money
-
         return s
+
+    def transfer_money(self, from_card: Card, to_card: Card, amount: float) -> bool:
+        if from_card.money >= amount:
+            from_card.money -= amount
+            to_card.money += amount
+            return True
+        return False
 
 
 types = [CardType("Humo"), CardType("Uzcard"), CardType("Visa")] 
 
-
-account = Account("ali", "vali", "+998881234567")
-c1 = AloqaBankCard('olyik', 2345123451234123, 566, datetime(2029, 12, 12), 100, types[0])
-c2 = SQBBankCard('bissness', 4132413241234, 435, datetime(2030, 12, 12), 50, types[2])
+account = Account("Ali", "Vali", "+998881234567")
+c1 = AloqaBankCard('Oylik', 2345123451234123, 566, datetime(2029, 12, 12), 100, types[0])
+c2 = SQBBankCard('Bussiness', 4132413241234, 435, datetime(2030, 12, 12), 50, types[2])
 
 account.add_card(c1)
 account.add_card(c2)
 
-
-def transfer_money(self, from_card: Card, to_card: Card, amount: float) -> bool:
-    if from_card.money >= amount:
-        from_card.money -= amount
-        to_card.money += amount
-        return True
-    return False
-
-Account.transfer_money = transfer_money
-
-
-print("1. Aloqa bank kartasi: ", c1.money)
-print("2.SQB bank kartasi: ", c2.money)
+print("1. Aloqa Bank Kartasi: ", c1.money)
+print("2. SQB Bank Kartasi: ", c2.money)
 print(f"Jami Balans: {account.get_balans()}")
 
-d=float(input("Qancha pul otkazmoqchisiz: "))
+d = float(input("Qancha pul otkazmoqchisiz? "))
 
-if account.get_balans() > 0:
+source_card_choice = int(input("Qaysi kartadan pul o'tkazmoqchisiz (1 - Aloqa, 2 - SQB): "))
 
-    c3 = NBUBankCard('personal', 1234567890123456, 123, datetime(2025, 5, 5), d, types[1])
-    account.add_card(c3)
+if source_card_choice == 1 and c1.money >= d:
+    target_card_choice = int(input("Pulni qaysi kartaga o'tkazmoqchisiz (1 - Aloqa, 2 - SQB): "))
+    if target_card_choice == 1:
+        print("Xatolik! O'zingizdan o'zingizga pul o'tkazolmasiz.")
+    elif target_card_choice == 2:
+        if account.transfer_money(c1, c2, d):
+            print("otkazma muvaffaqiyatli amalga oshirildi")
+        else:
+            print("Balansda yetarli mablag' yo'q")
+if source_card_choice == 1 and c1.money >= d:
+    target_card_choice = int(input("Pulni qaysi kartaga o'tkazmoqchisiz (1 - Aloqa, 2 - SQB): "))
+    if target_card_choice == 1:
+        print("Xatolik! O'zingizdan o'zingizga pul o'tkazolmasiz.")
+    elif target_card_choice == 2:
+        if account.transfer_money(c1, c2, d):
+            print("otkazma muvaffaqiyatli amalga oshirildi")
+        else:
+            print("Balansda yetarli mablag' yo'q")
 else:
     print("Balansda yetarli mablag' yo'q")
 
-if account.get_balans() > 0:
-    print("Transfer successful")
-    print(f"Jami Balans: {account.get_balans()}")
-else:
-    print("Balansda yetarli mablag' yo'q")
+print(f"Jami Balans: {account.get_balans()}")
+print("1. Aloqa Bank Kartasi: ", c1.money)
+print("2. SQB Bank Kartasi: ", c2.money)
